18448589001d2dde4fccea6019aa5c4c7b1f05bc
sumiya <sumikof187@gmail.com>
2025-01-05 22:45:39 +0900

    quit処理をfixtureの中に移した

--- tests/e2e/test_edge_browser.py
+++ tests/e2e/test_edge_browser.py
@@ -9,23 +9,21 @@ logger = logging.getLogger("test_logger")
 
 
 @pytest.fixture
-def driver():
-    return create_driver(EdgeDriver)
+def test_web_driver():
+    web_driver = create_driver(EdgeDriver)
+    yield web_driver
+    web_driver.quit()
+    logging.info("Edgeブラウザを閉じました")
 
 
-def test_edge_screenshot(driver, tmp_path):
-    try:
-        # 指定のURLを開く
-        driver.get("https://www.yahoo.co.jp/")
-        logging.info("Yahooのページを開きました")
+def test_edge_screenshot(test_web_driver, tmp_path):
+    # 指定のURLを開く
+    test_web_driver.get("https://www.yahoo.co.jp/")
+    logging.info("Yahooのページを開きました")
 
-        # ページタイトルを取得して確認
-        logging.info(f"ページタイトル: {driver.title}")
+    # ページタイトルを取得して確認
+    logging.info(f"ページタイトル: {test_web_driver.title}")
 
-        # 画面キャプチャを取得して保存
-        screenshot_path = os.path.join(tmp_path, "screenshot_full_headless.png")
-        driver.full_screenshot(screenshot_path)
-    finally:
-        # ブラウザを閉じる
-        driver.quit()
-        logging.info("Edgeブラウザを閉じました")
+    # 画面キャプチャを取得して保存
+    screenshot_path = os.path.join(tmp_path, "screenshot_full_headless.png")
+    test_web_driver.full_screenshot(screenshot_path)
--- tests/e2e/test_ie_browser.py
+++ tests/e2e/test_ie_browser.py
@@ -1,25 +1,29 @@
 import os
 import logging
 
+import pytest
+
 from src.driver_factory import IEDriver, create_driver
 
 logger = logging.getLogger("test_logger")
 
 
-def test_ie_screenshot(tmp_path):
-    driver = create_driver(IEDriver)
-    try:
-        # 指定のURLを開く
-        driver.get("https://www.wikipedia.org/")
-        logging.info("wikipediaのページを開きました")
+@pytest.fixture
+def test_web_driver():
+    web_driver = create_driver(IEDriver)
+    yield web_driver
+    web_driver.quit()
+    logging.info("IEブラウザを閉じました")
 
-        # ページタイトルを取得して確認
-        logging.info(f"ページタイトル: {driver.title}")
 
-        # 画面キャプチャを取得して保存
-        screenshot_path = os.path.join(tmp_path, "screenshot_full_headless.png")
-        driver.full_screenshot(screenshot_path)
-    finally:
-        # ブラウザを閉じる
-        driver.quit()
-        logging.info("IEブラウザを閉じました")
+def test_ie_screenshot(test_web_driver, tmp_path):
+    # 指定のURLを開く
+    test_web_driver.get("https://www.wikipedia.org/")
+    logging.info("wikipediaのページを開きました")
+
+    # ページタイトルを取得して確認
+    logging.info(f"ページタイトル: {test_web_driver.title}")
+
+    # 画面キャプチャを取得して保存
+    screenshot_path = os.path.join(tmp_path, "screenshot_full_headless.png")
+    test_web_driver.full_screenshot(screenshot_path)
